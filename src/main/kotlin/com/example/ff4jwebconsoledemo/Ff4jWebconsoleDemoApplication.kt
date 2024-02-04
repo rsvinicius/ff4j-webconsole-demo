14032d39b2aca3e3ef0b631fd387e85c616b3708
Vinicius R <vrodrigues.code@gmail.com>
2024-02-04 17:13:53 -0300

    First commit (Spring Initialzr)

---
+++ src/main/kotlin/com/example/ff4jwebconsoledemo/Ff4jWebconsoleDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ff4jwebconsoledemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ff4jWebconsoleDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<Ff4jWebconsoleDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ff4jwebconsoledemo/Ff4jWebconsoleDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ff4jwebconsoledemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ff4jWebconsoleDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
